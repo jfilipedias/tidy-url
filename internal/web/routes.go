93fcbd2ffdbe01fabcef9617bc2265f6d770a58a
Filipe Dias <filipediascontato@gmail.com>
2025-02-25 16:48:49 -0300

    feat: setup static files

--- internal/web/routes.go
+++ internal/web/routes.go
@@ -5,11 +5,13 @@ import (
 
 	"github.com/a-h/templ"
 	"github.com/go-chi/chi/v5"
-	"github.com/jfilipedias/tidy-url/ui/pages"
+	"github.com/jfilipedias/tidy-url/ui"
+	"github.com/jfilipedias/tidy-url/ui/page"
 )
 
 func routes() http.Handler {
 	r := chi.NewRouter()
-	r.Get("/", templ.Handler(pages.Home()).ServeHTTP)
+	r.Get("/", templ.Handler(page.Home()).ServeHTTP)
+	r.Get("/static/*", http.FileServerFS(ui.Files).ServeHTTP)
 	return r
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./ui/**/*.{go,js,templ,html}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
