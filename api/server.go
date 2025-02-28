71dd610d83f09b2cb7ba45a403b9efe7aed05275
Filipe Dias <filipediascontato@gmail.com>
2025-02-28 16:43:53 -0300

    feat: add graceful shutdown

--- api/server.go
+++ api/server.go
@@ -1,10 +1,14 @@
 package api
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/jfilipedias/tidy-url/url"
@@ -48,6 +52,37 @@ func (api *API) Serve() error {
 		ErrorLog:     slog.NewLogLogger(api.logger.Handler(), slog.LevelError),
 	}
 
+	errShutdown := make(chan error, 1)
+	go api.shutdown(srv, errShutdown)
+
 	api.logger.Info("starting server", "addr", srv.Addr)
-	return srv.ListenAndServe()
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-errShutdown
+	if err != nil {
+		return err
+	}
+
+	api.logger.Info("stopped server", "addr", srv.Addr)
+	return nil
+}
+
+func (api *API) shutdown(server *http.Server, errShutdown chan error) {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	s := <-quit
+
+	api.logger.Info("caught signal", "signal", s.String())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
+	if err != nil {
+		errShutdown <- err
+	}
+	errShutdown <- nil
 }
